24decb0d387b69ad572f8b18ebf04062ac497342
bbogum19 <berkaye2001@hotmail.com>
2023-02-22 19:59:45 +0300

    Added logo of project.

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, Image } from 'react-native';
 import styles from './Login.style';
 
 import Input from '../../components/Input';
@@ -12,12 +12,13 @@ const Login = () => {
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.logo_container}>
-                <Text>LOGO!</Text>
+                <Image style={styles.logo} source={require('../../../assets/login-logo.png')} />
+                {/* staticten bir resim cektiğimiz için require tercih edilir. Eğer ki endpointten bir resim cekecek olsaydık uri:... tercih edilirdi. */}
             </View>
             <View style={styles.body_container}>
                 <Input placeholder="Kullanıcı Adınızı Giriniz..." onChangeText={example} />
                 <Input placeholder="Şifrenizi Giriniz..." onChangeText={example} />
-                <Button text="Giriş Yap" />
+                <Button text="Giriş Yap" onPress={() => console.log("Giriş Yapıldı...")} />
             </View>
         </SafeAreaView>
     )
--- src/pages/Login/Login.style.js
+++ src/pages/Login/Login.style.js
@@ -1,10 +1,21 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 export default StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#4fc3f7',
     },
-    logo_container: {},
-    body_container: {}
+    logo: {
+        width: Dimensions.get('window').width * 0.9,
+        height: Dimensions.get('window').height / 3,
+        resizeMode: 'contain',
+        tintColor: '#f0f0f0' // png formattaki resimlere renk verebilmemize olanak sağlar
+    },
+    logo_container: {
+        flex:1,
+        justifyContent:'center'
+    },
+    body_container: {
+        flex:1,
+    }
 })
