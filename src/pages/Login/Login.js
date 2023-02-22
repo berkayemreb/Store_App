0d11320519ae20b8e39466220408280f53826d69
bbogum19 <berkaye2001@hotmail.com>
2023-02-22 18:54:50 +0300

    The skeleton structure of the login page has been created.

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { SafeAreaView, View, Text } from 'react-native';
+import styles from './Login.style';
+import Input from '../../components/Input';
 
 const Login = () => {
+    const example = (text) => {
+        console.log(text);
+    }
     return (
-        <SafeAreaView>
-            <Text>Login Page!</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.logo_container}>
+                <Text>LOGO!</Text>
+            </View>
+            <View style={styles.body_container}>
+                <Input placeholder="Kullanıcı Adınızı Giriniz..." onChangeText={example} />
+                <Input placeholder="Şifrenizi Giriniz..." onChangeText={example} />
+            </View>
         </SafeAreaView>
     )
 }
