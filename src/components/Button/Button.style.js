21c4e7c646ed8fc8f7ee3df7a9b5fa051334a880
bbogum19 <berkaye2001@hotmail.com>
2023-02-22 19:14:27 +0300

    The custom button component has been created.

---
+++ src/components/Button/Button.style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        borderWidth: 1,
+        borderRadius: 50,
+        borderColor:'#a0a0a0',
+        marginVertical: 5,
+        marginHorizontal: 10,
+        padding: 7,
+        alignItems: 'center',
+        backgroundColor: '#f0f0f0'
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: '800',
+        color: '#4fc3f7',
+    }
+})
