5020745eec95ce9d180bbefb0a3658959357d29a
bbogum19 <berkaye2001@hotmail.com>
2023-02-22 19:14:56 +0300

    The background color of the login page has been changed.

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { SafeAreaView, View, Text } from 'react-native';
 import styles from './Login.style';
+
 import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const Login = () => {
     const example = (text) => {
@@ -15,6 +17,7 @@ const Login = () => {
             <View style={styles.body_container}>
                 <Input placeholder="Kullanıcı Adınızı Giriniz..." onChangeText={example} />
                 <Input placeholder="Şifrenizi Giriniz..." onChangeText={example} />
+                <Button text="Giriş Yap" />
             </View>
         </SafeAreaView>
     )
--- src/pages/Login/Login.style.js
+++ src/pages/Login/Login.style.js
@@ -1,7 +1,10 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {},
+    container: {
+        flex: 1,
+        backgroundColor: '#4fc3f7',
+    },
     logo_container: {},
     body_container: {}
 })
