c1d3006a933fd5e8ea3b29c491dfe3f4b7d5729e
bbogum19 <berkaye2001@hotmail.com>
2023-02-27 15:05:45 +0300

    Transition to another page has been made from the login page.

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { SafeAreaView, View, Text, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, Text, Image, Alert } from 'react-native';
 import styles from './Login.style';
 
 import Input from '../../components/Input';
@@ -7,10 +7,26 @@ import Button from '../../components/Button';
 
 import { Formik } from 'formik';
 
-const Login = () => {
+import usePost from '../../hooks/usePost';
+import { URL_AUTH_API } from '@env';
+
+const Login = ({ navigation }) => {
+
+    const { data, loading, error, setError, post } = usePost();
 
     const onSubmit = (values) => {
-        console.log(values);
+        (values.username !== '' && values.password !== '')
+            ? post(URL_AUTH_API + '/login', values)
+            : Alert.alert('Dükkan', 'Kullanıcı adı ve şifre giriniz.');
+    }
+
+    if (error) {
+        Alert.alert('Dükkan', 'Kullanıcı adı veya şifre yanlış.');
+        setError(false);
+    }
+
+    if (data) {
+        navigation.navigate('ProductScreen');
     }
 
     return (
@@ -20,15 +36,15 @@ const Login = () => {
                 {/* staticten bir resim cektiğimiz için require tercih edilir. Eğer ki endpointten bir resim cekecek olsaydık uri:... tercih edilirdi. */}
             </View>
             <Formik
-                initialValues={{ userName: '', password: '' }}
+                initialValues={{ username: '', password: '' }}
                 onSubmit={onSubmit}
             >
                 {({ handleChange, values, handleSubmit }) => (
                     <View style={styles.body_container}>
                         <Input
                             placeholder="Kullanıcı Adınızı Giriniz..."
-                            onChangeText={handleChange('userName')}
-                            value={values.userName}
+                            onChangeText={handleChange('username')}
+                            value={values.username}
                             iconName='account'
                         />
                         <Input
@@ -38,7 +54,7 @@ const Login = () => {
                             iconName='key'
                             isSecure
                         />
-                        <Button text="Giriş Yap" onPress={handleSubmit} />
+                        <Button text="Giriş Yap" onPress={handleSubmit} loading={loading} />
                     </View>
                 )}
             </Formik>
@@ -49,3 +65,5 @@ const Login = () => {
 export default Login;
 
 // GENEL NOT: formik kullanmak yerine stateler ile de bu işlemi yapabilirdik ama her harf degişikliginde tüm component render edileceği için verimli olmazdı. Bu yuzden form yapılarında formik kullanmak daha kullanışlıdır.
+
+
