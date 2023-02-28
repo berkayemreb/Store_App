5a0992d6802f9a87bcb204bcb4a1765daaac7008
bbogum19 <berkaye2001@hotmail.com>
2023-02-28 14:14:43 +0300

    The user information has been saved to the phone memory and a page transition has been made.

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, View, Text, Image, Alert } from 'react-native';
+import React from 'react';
+import { SafeAreaView, View, Image, Alert } from 'react-native';
 import styles from './Login.style';
 
 import Input from '../../components/Input';
@@ -10,9 +10,12 @@ import { Formik } from 'formik';
 import usePost from '../../hooks/usePost';
 import { URL_AUTH_API } from '@env';
 
+import { useDispatch } from 'react-redux';
+
 const Login = ({ navigation }) => {
 
     const { data, loading, error, setError, post } = usePost();
+    const dispatch = useDispatch();
 
     const onSubmit = (values) => {
         (values.username !== '' && values.password !== '')
@@ -26,7 +29,7 @@ const Login = ({ navigation }) => {
     }
 
     if (data) {
-        navigation.navigate('ProductScreen');
+        dispatch({ type: 'SET_USER', payload: { user } })
     }
 
     return (
@@ -63,6 +66,29 @@ const Login = ({ navigation }) => {
 }
 
 export default Login;
+
+const user = {
+    "address": {
+        "geolocation": {
+            "lat": "-37.3159",
+            "long": "81.1496"
+        },
+        "city": "kilcoole",
+        "street": "new road",
+        "number": 7682,
+        "zipcode": "12926-3874"
+    },
+    "id": 1,
+    "email": "john@gmail.com",
+    "username": "johnd",
+    "password": "m38rmF$",
+    "name": {
+        "firstname": "john",
+        "lastname": "doe"
+    },
+    "phone": "1-570-236-7033",
+    "__v": 0
+}
 
 // GENEL NOT: formik kullanmak yerine stateler ile de bu işlemi yapabilirdik ama her harf degişikliginde tüm component render edileceği için verimli olmazdı. Bu yuzden form yapılarında formik kullanmak daha kullanışlıdır.
 
