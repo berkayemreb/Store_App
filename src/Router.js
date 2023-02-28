38b426da05c0991f97dfa6c2ec15f8ef13dd128e
bbogum19 <berkaye2001@hotmail.com>
2023-02-28 14:16:04 +0300

    The Redux provider structure has been made to cover the entire project."

--- src/Router.js
+++ src/Router.js
@@ -6,40 +6,54 @@ import Product from './pages/Product';
 import Detail from './pages/Detail';
 import Login from './pages/Login';
 
+import { useSelector } from 'react-redux';
+import LoadingAnimation from './components/LoadingAnimation';
+
 const Stack = createNativeStackNavigator();
 
 const Router = () => {
 
+  const userSession = useSelector(s => s.user);
+  const isAuthLoading = useSelector(s => s.isAuthLoading);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name='LoginScreen'
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name='ProductScreen'
-          component={Product}
-          options={{
-            title: 'Shop',
-            headerTitleStyle: { color: '#fff', fontSize: 24 },
-            headerTitleAlign: 'center',
-            headerStyle: { backgroundColor: '#4fc3f7' }
-          }} />
-        <Stack.Screen
-          name='DetailScreen'
-          component={Detail}
-          options={{
-            title: 'Detail',
-            headerTitleStyle: { color: '#fff', fontSize: 24 },
-            headerTitleAlign: 'center',
-            headerStyle: { backgroundColor: '#4fc3f7' },
-            headerTintColor: '#fff'
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+      {isAuthLoading ? (
+        <LoadingAnimation />
+      ) :
+        !userSession ? (
+          <Stack.Navigator>
+            <Stack.Screen
+              name='LoginScreen'
+              component={Login}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        ) : (
+          <Stack.Navigator>
+            <Stack.Screen
+              name='ProductScreen'
+              component={Product}
+              options={{
+                title: 'Shop',
+                headerTitleStyle: { color: '#fff', fontSize: 24 },
+                headerTitleAlign: 'center',
+                headerStyle: { backgroundColor: '#4fc3f7' }
+              }} />
+            <Stack.Screen
+              name='DetailScreen'
+              component={Detail}
+              options={{
+                title: 'Detail',
+                headerTitleStyle: { color: '#fff', fontSize: 24 },
+                headerTitleAlign: 'center',
+                headerStyle: { backgroundColor: '#4fc3f7' },
+                headerTintColor: '#fff'
+              }}
+            />
+          </Stack.Navigator>
+        )}
+    </NavigationContainer >
   )
 }
 
